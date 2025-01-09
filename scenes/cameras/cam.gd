3f3ac7a744e8fface5faf1d1e8538181135d8f94
Adam Wood <wood@badcircle.com>
2025-01-08 20:22:19 -0600

    added sax

--- scenes/cameras/cam.gd
+++ scenes/cameras/cam.gd
@@ -3,13 +3,30 @@ extends Node3D
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	toggle_cursor()
+	handle_zoom()
 
 func _input(event: InputEvent) -> void:
 	if event is InputEventMouseMotion and Input.is_action_pressed("rotate_camera"):
 		rotation_degrees.y += event.relative.x * -0.1
+
+func toggle_cursor() -> void:
+	if Input.is_action_just_released("toggle_cursor"):
+		if Input.mouse_mode == Input.MOUSE_MODE_CAPTURED:
+			Input.mouse_mode = Input.MOUSE_MODE_VISIBLE
+		else:
+			Input.mouse_mode = Input.MOUSE_MODE_CAPTURED
+
+func handle_zoom() -> void:
+	if Input.is_action_just_released("zoom_in"):
+		#if $Cam.position.z < 100:
+		$Cam.translate_object_local(Vector3(0, 0, -1))
+	if Input.is_action_just_released("zoom_out"):
+		#if $Cam.position.z > 0:
+		$Cam.translate_object_local(Vector3(0, 0, 1))
+	#$Cam.position.z = clamp($Cam.position.z, -2, 100)
---
+++ scenes/instruments/sax/sax.gd
@@ -0,0 +1,45 @@
+class_name Sax extends Node3D
+
+@onready var A : AudioStreamPlayer = $"Sax-A"
+@onready var C : AudioStreamPlayer = $"Sax-C"
+@onready var D : AudioStreamPlayer = $"Sax-D"
+@onready var F : AudioStreamPlayer = $"Sax-F"
+@onready var G : AudioStreamPlayer = $"Sax-G"
+
+@onready var notes : Array[AudioStreamPlayer] = [A, C, D, F, G]
+
+var pitch_scale : float = 1.0
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	play_notes()
+	shift_pitch(delta)
+	update_label()
+
+func play_notes() -> void:
+	if Input.is_action_just_pressed("play_A"):
+		A.playing = true
+	if Input.is_action_just_pressed("play_C"):
+		C.playing = true
+	if Input.is_action_just_pressed("play_D"):
+		D.playing = true
+	if Input.is_action_just_pressed("play_F"):
+		F.playing = true
+	if Input.is_action_just_pressed("play_G"):
+		G.playing = true
+
+func shift_pitch(diff) -> void:
+	var shiftDir = Input.get_axis("shift_pitch_down", "shift_pitch_up") + 1.0
+	pitch_scale = move_toward(pitch_scale, shiftDir, diff * PI * PI)
+	pitch_scale = clamp(pitch_scale, 0.5, 1.5)
+	for note in notes:
+		note.pitch_scale = pitch_scale
+		note.max_polyphony = 16
+
+func update_label() -> void:
+	$Label.text = "Pitch: %.2f" % pitch_scale
